87dcf66c077b16f33761f20bc8484bbdbbe9bdf8
ogola5 <ogolaevance5@gmail.com>
2022-11-07 12:19:53 +0300

    completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
+    belong_to :actor
+    belong_to :show
+    def say_that_thing_you_say
+        "#{self.name} always say #{self.catchphrase}"
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :Network
+
+    def actor_list
+        characters.map do |character|
+            character.actor.full_name
+        end
+    end
   
 end
